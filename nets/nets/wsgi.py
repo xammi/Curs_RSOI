0c9cb4cb947913ea2f671b0f273613c98d9fc6b3
Kislenko Maksim <kmg.xammi.1@gmail.com>
2017-03-20 19:12:40 +0300

    All services

--- nets/nets/wsgi.py
+++ nets/nets/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ann_service.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nets.settings")
 
 application = get_wsgi_application()
--- oauth/oauth/wsgi.py
+++ oauth/oauth/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for curs_rsoi project.
+WSGI config for oauth project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "central.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oauth.settings")
 
 application = get_wsgi_application()
